950eb25902ba377684dab1dd412e70313cbaee93
Brook <christianlopezrivera0@gmail.com>
2024-02-27 17:01:07 -0400

    updating test model with task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import uuid
 from datetime import datetime
 
@@ -6,13 +7,20 @@ class BaseModel:
     """
     BaseModel class defines common attributes/methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
@@ -35,3 +43,4 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
